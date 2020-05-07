f7317bd5dc1e6341ba565abb3381d00798b7d4cd
joergviola <joerg@joergviola.de>
2020-05-07 19:42:49 +0200

    Fixed task timeline

--- ui/src/views/task/Form.vue
+++ ui/src/views/task/Form.vue
@@ -2,10 +2,10 @@
   <div class="components-container">
     <h2>{{task.name}}</h2>
     <el-tabs v-model="activeTab">
-      <el-tab-pane key="1" label="Details" name="1">
+      <el-tab-pane lazy key="1" label="Details" name="1">
         <task-details :id="$route.params.id" :tid="$route.params.tid" v-on:update="t => task = t" />
       </el-tab-pane>
-      <el-tab-pane key="2" label="Timeline" name="2">
+      <el-tab-pane lazy key="2" label="Timeline" name="2">
         <task-timeline :id="$route.params.id" :task="task" />
       </el-tab-pane>
     </el-tabs>
