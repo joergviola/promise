97203e62aa872c9d7287c66cff3d003cb50f1c16
joergviola <joerg@joergviola.de>
2020-05-13 08:34:08 +0200

    User password

--- ui/src/components/Generic/Fields.vue
+++ ui/src/components/Generic/Fields.vue
@@ -1,21 +1,18 @@
 <template>
   <div>
     <el-form-item v-for="(field,i) in fields" :key="i" :label="field.label">
-      <el-input v-if="!field.type" :disabled="field.disabled" v-model="item[field.name]" type="text" >
-        <template v-if="field.postfix" slot="append">{{field.postfix}}</template>
-      </el-input>
       <el-input v-if="field.type=='textarea'" v-model="item[field.name]" :rows="1" type="textarea" autosize :placeholder="field.placeholder" />
-      <el-select v-if="field.type=='select'" v-model="item[field.name]">
+      <el-select v-else-if="field.type=='select'" v-model="item[field.name]">
         <el-option v-for="(o, i) in field.options" :key="i" :label="o" :value="o" />
       </el-select>
       <el-date-picker
-        v-if="field.type=='date'"
+        v-else-if="field.type=='date'"
         v-model="item[field.name]"
         :type="field.type"
         value-format="yyyy-MM-dd"
       />
       <to-one
-        v-if="field.type=='to-one'"
+        v-else-if="field.type=='to-one'"
         v-model="item[field.name]"
         :type="field.ref"
         :display="field.display"
@@ -23,6 +20,10 @@
         :query="field.query"
         @input="id => field.input ? field.input(id) : null"
       />
+      <el-input v-else-if="field.type=='password'" show-password v-model="item[field.name]" />
+      <el-input v-else type="text" :disabled="field.disabled" v-model="item[field.name]"  >
+        <template v-if="field.postfix" slot="append">{{field.postfix}}</template>
+      </el-input>
     </el-form-item>
   </div>
 </template>
--- ui/src/views/users/Form.vue
+++ ui/src/views/users/Form.vue
@@ -20,6 +20,7 @@ export default {
         { name: 'name', label: 'Name' },
         { name: 'email', label: 'E-Mail' },
         { name: 'phone', label: 'Phone' },
+        { name: 'password', label: 'Password', type: 'password' },
         { name: 'role_id', label: 'Role', type: 'to-one', ref: 'role', display: 'name'},
         { name: 'organisation_id', label: 'Organsisation', type: 'to-one', ref: 'organisation', display: 'name' },
       ],
