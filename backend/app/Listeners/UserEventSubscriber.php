526522a04dc86a4bd70049e03dc9501ebd11527f
joergviola <joerg@joergviola.de>
2020-05-22 10:36:38 +0200

    Probem fixed Password

--- backend/app/Listeners/UserEventSubscriber.php
+++ backend/app/Listeners/UserEventSubscriber.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\ApiBeforeCreateEvent;
 use App\Events\ApiAfterReadEvent;
 use App\Events\ApiBeforeUpdateEvent;
+use App\PermissionException;
 use Illuminate\Support\Facades\Hash;
 
 class UserEventSubscriber
@@ -22,22 +23,10 @@ class UserEventSubscriber
         if ($event->type!='users') return;
 
         if (!empty($event->item['password'])) {
-            $this->checkOldPassword($event);
-            unset($event->item['old_password']);
             $event->item['password'] = Hash::make($event->item['password']);
         } else {
             unset($event->item['password']);
         }
-    }
-
-    private function checkOldPassword($event) {
-        if (isset($event->item['old_password'])) {
-            $hash = Hash::make($event->item['old_password']);
-            if ($hash==Auth::user()->password) {
-                return;
-            }
-        }
-        throw new PermissionException("Unable to change password");
     }
 
     public function handleCreate(ApiBeforeCreateEvent $event) {
