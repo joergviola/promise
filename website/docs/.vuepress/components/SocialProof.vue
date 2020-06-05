9ab9841e16cf1a62dcf3c3ae51773e38af15bf6f
joergviola <joerg@joergviola.de>
2020-06-05 08:56:39 +0200

    Fix

--- website/docs/.vuepress/components/SocialProof.vue
+++ website/docs/.vuepress/components/SocialProof.vue
@@ -1,32 +1,32 @@
 <template>
-  <section class="bg-white border-b pt-24 sm:pt-64 sm:mt-48 pb-12 h-full">
+  <section class="bg-white border-b pt-24 sm:pt-64 sm:mt-64 pb-12 h-full">
     <div class="container mx-auto pb-12" >
       <h2 class="w-full my-2 text-xl font-black leading-tight text-center text-gray-800 lg:mt-8" >
-        Replaces
+        Replaces:
       </h2>
 
       <div class="flex flex-1 flex-wrap max-w-4xl mx-auto items-center justify-between text-xl text-gray-500 font-bold opacity-75" >
         <span class="w-1/2 p-4 md:w-auto flex items-center">
-          - Pipedrive
+          Pipedrive
         </span>
         <span class="w-1/2 p-4 md:w-auto flex items-center">
-          - Asana
+          Asana
         </span>
         <span class="w-1/2 p-4 md:w-auto flex items-center">
-          - Trello
+          Trello
         </span>
         <span class="w-1/2 p-4 md:w-auto flex items-center">
-          - Harvest
+          Harvest
         </span>
         <span class="w-1/2 p-4 md:w-auto flex items-center">
-          - Toggle
+          Toggle
         </span>
         <span class="w-1/2 p-4 md:w-auto flex items-center">
-          - Moco
+          Moco
         </span>
       </div>
-      <div class="text-center text-gray-800">
-        ... and by integrating all this data, you get never-seen insights on all your project pitfalls:<br>
+      <div class="text-center text-gray-500">
+        ... and by integrating all this data, you get never-seen insights on all your project pitfalls!
       </div>
     </div>
   </section>
