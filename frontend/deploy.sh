1d6b4c8237e5cb4db3b282e2cea50ded1a19911d
joergviola <joerg@joergviola.de>
2020-06-11 13:28:01 +0200

    frontend von /app nach /

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -13,5 +13,5 @@ echo "Updating backend..."
 $SSH "cd $INSTALL && git pull && cd backend && composer install && php artisan migrate"
 
 echo "Deploying frontend..."
-$SSH "cd $INSTALL/backend/public/app && rm -rf *"
-$SCP -r ../backend/public/app/* "$USER@$HOST:$INSTALL/backend/public/app"
+$SSH "cd $INSTALL/backend/public && rm -rf *"
+$SCP -r ../backend/public/* "$USER@$HOST:$INSTALL/backend/public"
