7f2fa36d22790751cf7d52c18698d5d270daa6b3
joergviola <joerg@joergviola.de>
2020-06-12 12:34:16 +0200

    Header layout

--- frontend/src/components/layout/Header.vue
+++ frontend/src/components/layout/Header.vue
@@ -1,26 +1,30 @@
 <template>
-    <div style="display: flex;">
-        <div style="width: 150px;" >
+    <div style="display: flex; justify-content: flex-start;">
+        <div style="width: 150px" >
             <img height="50px" style="margin: 5px" src="@/assets/img/logo.png" />
         </div>
-        <el-breadcrumb separator-class="el-icon-arrow-right" style="height: 60px; line-height: 60px">
-            <el-breadcrumb-item 
-                v-for="(m,i) in $route.matched"
-                :to="breadcrumbPath(i)"
-                :key="i"
-            >{{m.name}}</el-breadcrumb-item>
-        </el-breadcrumb>            
-        <el-dropdown style="height: 60px; line-height: 60px; margin-left: auto;margin-right: 25px">
-            <avatar :user="user" cls="header-avatar"/>
-            <span class="el-dropdown-link text-right">
-                {{user.name}}
-                <i class="el-icon-arrow-down el-icon--right"></i>
-            </span>
-            <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item><a @click.prevent="profile">Profile</a></el-dropdown-item>
-                <el-dropdown-item><a @click.prevent="logout">Logout</a></el-dropdown-item>
-            </el-dropdown-menu>
-        </el-dropdown>
+        <div  style="flex-grow: 1; display: flex; justify-content: space-between; align-items: stretch;">
+            <el-breadcrumb separator-class="el-icon-arrow-right" style="height: 60px; line-height: 60px">
+                <el-breadcrumb-item 
+                    v-for="(m,i) in $route.matched"
+                    :to="breadcrumbPath(i)"
+                    :key="i"
+                >{{m.name}}</el-breadcrumb-item>
+            </el-breadcrumb>            
+            <div style="text-align: right;">
+                <avatar :user="user" cls="header-avatar"/>
+                <el-dropdown style="height: 60px; line-height: 60px; margin-left: auto;margin-right: 25px">
+                    <span class="el-dropdown-link text-right">
+                        {{user.name}}
+                        <i class="el-icon-arrow-down el-icon--right"></i>
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item><a @click.prevent="profile">Profile</a></el-dropdown-item>
+                        <el-dropdown-item><a @click.prevent="logout">Logout</a></el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+            </div>
+        </div>
     </div>
 </template>
 
