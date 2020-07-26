6ce5178d444f47752432037c7e75a4f40cc87d7e
joergviola <joerg@joergviola.de>
2020-07-26 19:26:00 +0200

    Default layout changed

--- frontend/src/layouts/Default.vue
+++ frontend/src/layouts/Default.vue
@@ -1,23 +1,21 @@
 <template>
-    <el-container class="grid">
-        <el-header class="header">
+    <div class="grid">
+        <div class="header">
             <app-header />
-        </el-header>
-        <el-container>
-            <el-aside class="sidebar" width="150px">
+        </div>
+            <div class="sidebar">
                 <router-menu />
                 <router-menu :bottom="true" />
                 <div style="text-align: center; font-size: 70%; padding-bottom: 10px">
                     <router-link to="/version"><span>gluon 21.07.20</span></router-link>
                 </div>
-            </el-aside>
-            <el-main class="content">
+            </div>
+            <div class="content">
                 <transition name="content">
                     <router-view ></router-view>
                 </transition>
-            </el-main>
-        </el-container>
-    </el-container>
+            </div>
+    </div>
 </template>
 
 <script>
@@ -34,9 +32,14 @@ export default {
 </script>
 
 <style scoped>
+
 .grid {
-  grid-template-columns: 150px auto;
-  grid-template-rows: 60px auto;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  display: grid;
+  grid-template-columns: 150px 1fr;
+  grid-template-rows: 60px 1fr;
   grid-template-areas: 
     "header header"
     "sidebar content";
@@ -44,16 +47,27 @@ export default {
 
 .grid .header {
     grid-area: header;
-    border-bottom: 1px solid #DDDDDD; padding: 0
+    border-bottom: 1px solid #DDDDDD; 
+    padding: 0;
+    box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
 }
 
 .grid .sidebar {
     grid-area: sidebar;
     background-color:#666666;
+    display: flex; 
+    flex-direction: column; 
+    justify-content: space-between;
+}
+
+.grid .sidebar a {
+   color: white;
 }
 
 .grid .content {
     grid-area: content;
+    overflow: scroll;
+    padding: 20px;
 }
 
 </style>
